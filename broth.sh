3c4f4d1d38f972535a5878cf6fa63f92cc17df7b
aym3ric <aym3ric>
2009-04-13 22:14:53 +0000

    lh config test + old config as temp reminders

--- broth.sh
+++ broth.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 #broth.sh - the base of all soups
 
-echo "awesome"
+if [ -d .svn ]; then
+    echo "don't run me from SVN! export me somewhere else"
+    exit
+fi
+
+
+
+echo "broth - the mother of all soups"
+
+lh config \
+    --mirror-bootstrap "" \
+    --mirror-chroot "" \
+    --binary-indices disabled
+
