32d840a4d4e9b5c27febba0c7882f0807f4809a6
aym3ric <aym3ric>
2009-05-16 15:51:34 +0000

    done importing binary settings in binary

--- broth.sh
+++ broth.sh
@@ -3,7 +3,7 @@
 
 BUILDER=`whoami`
 BUILD_DIRECTORY="/home/$BUILDER/puredyne-build"
-
+PUREDYNE_VERSION="pure:dyne carrot&coriander"
 
 #if [ -d .svn ]; then
 #    echo "don't run me from SVN! export me somewhere else"
@@ -23,7 +23,18 @@ brothconfig() {
 lh config \
     --mirror-bootstrap "" \
     --mirror-chroot "" \
-    --binary-indices disabled
+    --binary-indices disabled \
+    --bootappend-live "persistent" \
+    --debian-installer-distribution "lenny" \
+    --hostname "puredyne" \
+    --iso-application "pure:dyne team" \
+    --iso-preparer "live-helper $VERSION" \
+    --iso-publisher "pure:dyne team; http://puredyne.goto10.org; puredyne-team@goto10.org"
+    --iso-volume $PUREDYNE_VERSION \
+    --syslinux-splash "splash.rle" \
+    --syslinux-timeout "10" \
+    --syslinux-menu "enabled" \
+    --username "lintian"
 }
 
 
