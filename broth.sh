6a7784c68178261f58cba9af945f4163c503dafa
aym3ric <aym3ric>
2009-05-16 18:32:35 +0000

    fixed syslinux splash path

--- broth.sh
+++ broth.sh
@@ -39,7 +39,7 @@ lh config \
     --iso-preparer "live-helper $VERSION" \
     --iso-publisher "pure:dyne team; http://puredyne.goto10.org; puredyne-team@goto10.org" \
     --iso-volume $PUREDYNE_VERSION \
-    --syslinux-splash "splash.rle" \
+    --syslinux-splash "binary_syslinux/splash.rle" \
     --syslinux-timeout "10" \
     --syslinux-menu "enabled" \
     --username "lintian"
