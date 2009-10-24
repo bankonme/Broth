7e6a5c05a64917a9a5dda33b7903662c00b95c5e
lintian <lintian@ubik-laptop>
2009-10-24 19:59:40 +0300

    some cleaning. they previous one was quite useless

--- stock/chroot_local-includes/sbin/make-puredyne-usb.sh
+++ stock/chroot_local-includes/sbin/make-puredyne-usb.sh
@@ -1,36 +1,61 @@
 #!/bin/bash 
 #DEVICE=/dev/sda
-DIR=/home/anton
+DIR=$HOME
+MBR=puredyne-usb-mbr
+
 cd $DIR
 
 if [ -z "$1" ] ; then
-    echo Usage: $0 DEVICE
-    echo Make sure you do this on the rigt device!!!!!!!!!!!!!!!!!!
-    echo WILL WIPE YOUR HARDDRIVE with no WARNING!!!!!!!!!!!!!!!!!!
-fdisk -l
-    echo '************************************************************************'
+    echo Usage: $0 [broth iso] [DEVICE] 
+    echo Make sure you do this on the right device!
+    echo WILL WIPE YOUR HARDDRIVE with no WARNING!!
+    echo 'You need the MBR for the key in' $DIR/$MBR    
+    echo ''
+fdisk -l|grep Disk
+    
+    echo 'select your usb device from above'
 exit 0
 
 fi
 
 
-echo 'making USB bootable cd on ' $1
+cd $DIR
+echo 'copy partition table.'
+dd of=$2 if=$MBR bs=512 count=1
+sudo mkfs.vfat -n puredyne $2'1'
+echo 'do some stuff'
 
-echo 'waiting for usb to settle...' 
-sleep 5
+sudo /sbin/lilo -M $2
+mkdir -p $DIR/iso $DIR/usb
 
-echo "* copy partition table on the key"
-dd of=$1 if=puredyne-usb-mbr bs=512 count=1
-echo 'done.'
+echo 'mount iso and key'
+sudo mount $2'1' $DIR/usb
+sudo mount -o loop $1 $DIR/iso
 
-echo '** copy over system partition data'
-dd if=puredyne-partition1 of=$1'1' bs=512
-echo 'done..'
-read -p  "*** Remove key and then back in. Press any key to continue!"
-sleep 3
-echo '**** format data partition'
-#mkfs.vfat -F 16 /dev/sda1                                                                           
-mkfs.ext2 -L live-rw $1'2'
+echo 'copying files...'
+sudo cp -av $DIR/iso/* $DIR/usb/
+# dont forget to copy .disk b/rother!
+echo 'changing bits and bytes'
+cd $DIR/usb
+sudo mv isolinux/* .
+sudo rm -rf isolinux/
+sudo sed -i 's/\/isolinux//g' *.cfg
+sudo sed -i 's/\/isolinux//g' *.txt
+sudo mv isolinux.cfg syslinux.cfg
+sudo mv isolinux.bin syslinux.bin
+
+echo 'clealypamaining up'
+cd ..
+sudo umount $DIR/usb
+sudo umount $DIR/iso
+
+echo 'running syslinux'
+syslinux $2'1'
+
+echo 'formating  data partition'
+
+sudo mkfs.ext2 -L live-rw $2'2'
 
 echo '***** done!'
+
 exit 0
