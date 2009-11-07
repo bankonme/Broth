76a4b117ad2f2dc48fe04d1f1cdbad15e06a64e6
Aymeric Mansoux <aymeric@goto10.org>
2009-11-07 13:50:34 +0100

    moved group settings to adduser.conf hook

--- stock/chroot_local-hooks/80-tweak_adduser_configuration.sh
+++ stock/chroot_local-hooks/80-tweak_adduser_configuration.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # This hook makes some changes on the default adduser.conf.
-# At the moment it only used to switch to zshell for all new users, 
+# At the moment it is used to switch the shell to zshell for all new users, 
 # including the live user because this one is created at boot time.
-# If needed this hook could be used to tweak a few more things, such
-# as default groups.
+# It also sets default extra groups for new users.
 
 # We want Zsh!
 sed -i 's/DSHELL=\/bin\/bash/DSHELL=\/bin\/zsh/g' /etc/adduser.conf
 
-
+# We want awesome groups!
+cat >> /etc/adduser.conf <<EOF
+EXTRA_GROUPS="dialout cdrom floppy plugdev netdev audio video users games"
+ADD_EXTRA_GROUPS=1
+EOF
