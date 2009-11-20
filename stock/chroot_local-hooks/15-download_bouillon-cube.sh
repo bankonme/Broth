de26a4fd06860f2f9393b3acae4280c26b08cf2e
Aymeric Mansoux <aymeric@goto10.org>
2009-11-20 13:13:47 +0100

    hook to doanload bouilloncube

---
+++ stock/chroot_local-hooks/15-download_bouillon-cube.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This hook will download the latest snapshot of some bouillon cube files
+# This is needed until bouillon cube is mature enough to be packaged properly
+
+bzr co lp:bouilloncube
+mv bouilloncube/sh/make-usb-key.sh /usr/sbin
+rm -rf bouilloncube
