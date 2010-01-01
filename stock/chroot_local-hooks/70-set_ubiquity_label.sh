22c3f06b4ae83c87f0c9349bfdedb25472a4ddad
Dan Stowell <danstowell+launch@gmail.com>
2010-01-01 16:00:51 +0000

    fix typo

--- stock/chroot_local-hooks/70-set_ubiquity_label.sh
+++ stock/chroot_local-hooks/70-set_ubiquity_label.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 
 # Replace the "RELEASE" keyword in the ubiquity menu item so it says "Install puredyne"
-sed s/RELEASE/puredyne/g /usr/share/applications/ubiquity-gtkui.desktop > ubiquity-gtkui.desktop.tmp && mv -f ubiquity-gtkui.desktop.tmp /usr/share/spplications/ubiquity-gtkui.desktop
+sed s/RELEASE/puredyne/g /usr/share/applications/ubiquity-gtkui.desktop > ubiquity-gtkui.desktop.tmp && mv -f ubiquity-gtkui.desktop.tmp /usr/share/applications/ubiquity-gtkui.desktop
 
