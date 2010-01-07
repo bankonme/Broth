4eb71d4dd51b4abf0b456fad0acad6e239da2959
Aymeric Mansoux <aym3ric@builder>
2010-01-07 17:51:40 +0000

    target test for 35-patchage_category.sh

--- stock/chroot_local-hooks/35-patchage_category.sh
+++ stock/chroot_local-hooks/35-patchage_category.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo "11c11
+if [ -e "/usr/share/applications/patchage.desktop" ]
+then
+    echo "11c11
 < Categories=Application;AudioVideo;
 ---
 > Categories=Application;AudioVideo;Audio;" | patch --no-backup-if-mismatch --forward /usr/share/applications/patchage.desktop
-
+fi
