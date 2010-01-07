76596f0ad5f996203e57b94a5a3476003d05ca31
Aymeric Mansoux <aym3ric@builder>
2010-01-07 18:02:24 +0000

    target test for 35-nted_category.sh

--- stock/chroot_local-hooks/35-nted_category.sh
+++ stock/chroot_local-hooks/35-nted_category.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# Removing the Audio category ensures nted stays in the Multimedia category
 
-# Removing the Audio category ensures mplayer stays in the Multimedia category
-echo "7c7
+if [ -e "/usr/share/applications/nted.desktop" ]
+then
+    echo "7c7
 < Categories=GNOME;Application;AudioVideo;
 ---
 > Categories=GNOME;Application;AudioVideo;Audio;" | patch --no-backup-if-mismatch --forward /usr/share/applications/nted.desktop
-
+fi
