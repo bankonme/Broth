521e4aa7d5b695d943f4e7894a83d502b1bb97cc
Aymeric Mansoux <aym3ric@builder>
2010-01-08 00:17:18 +0000

    removed mplayer desktop hook because we use th -nogui version

--- stock/chroot_local-hooks/35-mplayer_category.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Removing the Audio category ensures mplayer stays in the Multimedia category
-echo "17c17
-< Categories=GTK;AudioVideo;Audio;Video;Player;TV;
---
-> Categories=GTK;AudioVideo;Video;Player;TV;" | patch --no-backup-if-mismatch --forward /usr/share/applications/mplayer.desktop
-
