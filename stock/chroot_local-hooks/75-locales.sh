c050929c49ccd77e8de1cfbef63f875fc9637987
Aymeric Mansoux <aym3ric@builder>
2010-01-08 23:22:14 +0000

    locales selection without languages pack

---
+++ stock/chroot_local-hooks/75-locales.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# A selection of locales we want at boot time
+
+locale-gen de_DE.UTF-8
+locale-gen en_US.UTF-8
+locale-gen en_GB.UTF-8
+locale-gen es_ES.UTF-8
+locale-gen fr_FR.UTF-8
+locale-gen ru_RU.UTF-8
