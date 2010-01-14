747cbd503fa9f6c4f564d8a5f1770a63e230882e
Aymeric Mansoux <aymeric@goto10.org>
2010-01-14 18:00:23 +0100

    updated copyright year

--- broth.sh
+++ broth.sh
@@ -8,7 +8,7 @@
 #
 # ---
 #
-#    Copyright (C) 2008-2009  Puredyne Team
+#    Copyright (C) 2008-2010  Puredyne Team
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
