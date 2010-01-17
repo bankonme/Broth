5e22f5f2a5ad937c25938a5aaccf2f5265700c16
Aymeric Mansoux <aymeric@goto10.org>
2010-01-17 01:19:08 +0100

    correctly generate all the locales we need - closes #508157

--- stock/chroot_local-hooks-common/75-locales.sh
+++ stock/chroot_local-hooks-common/75-locales.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # A selection of locales we want at boot time
 
-locale-gen de_DE.UTF-8
-locale-gen en_US.UTF-8
-locale-gen en_GB.UTF-8
-locale-gen es_ES.UTF-8
-locale-gen fr_FR.UTF-8
-locale-gen ru_RU.UTF-8
+locale-gen de_DE.UTF-8 en_US.UTF-8 en_GB.UTF-8 es_ES.UTF-8 fr_FR.UTF-8 ru_RU.UTF-8
