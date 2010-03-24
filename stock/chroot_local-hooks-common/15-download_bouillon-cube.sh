d0f5b51e48c61a6bf30b6996dd00486931da44b2
Aymeric Mansoux <aymeric@goto10.org>
2010-03-25 00:17:40 +0200

    added grub2 config needed for make-live-device.sh

--- stock/chroot_local-hooks-common/15-download_bouillon-cube.sh
+++ stock/chroot_local-hooks-common/15-download_bouillon-cube.sh
@@ -4,4 +4,6 @@
 
 bzr co lp:bouilloncube
 mv bouilloncube/sh/grub2/make-live-device.sh /usr/sbin
+mkdir -p /usr/share/bouilloncube/sh/grub2
+mv bouilloncube/sh/grub2/grub.cfg.in /usr/share/bouilloncube/sh/grub2
 rm -rf bouilloncube
