075d302d25ea5606a26393ea140512f4a06d11c4
Dan Stowell <danstowell+launch@gmail.com>
2010-03-25 11:06:07 +0000

    Try cinelerra install easy way; oh and also fix its desktop file

--- stock/chroot_local-hooks-puredyne-DVD/30-cinelerra_install.sh
+++ stock/chroot_local-hooks-puredyne-DVD/30-cinelerra_install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-# broth/stock/chroot_local-hooks/30-cinelerra_install.sh
-# Shitty hack to trick dpkg to install cinelerra from the akirad repos in the chroot.
-
-# dependencies
-aptitude --assume-yes install optlibx11-noxcb-data optlibx11-noxcb-6 libguicastcv libmpeg3cv libquicktimecv akiradnews
-
-apt-get install --assume-yes --download-only cinelerracv
-dpkg --unpack /var/cache/apt/archives/cinelerracv*
-
-# yes, we remove it even if not triggered otherwise it will be a problem as soon as
-# aptitude or dpkg -a (etc..) are called while still in the chroot.
-grep -Ev shmmax /var/lib/dpkg/info/cinelerracv.postinst > tmpcine
-mv tmpcine /var/lib/dpkg/info/cinelerracv.postinst
+## broth/stock/chroot_local-hooks/30-cinelerra_install.sh
+## Shitty hack to trick dpkg to install cinelerra from the akirad repos in the chroot.
+#
+## dependencies
+#aptitude --assume-yes install optlibx11-noxcb-data optlibx11-noxcb-6 libguicastcv libmpeg3cv libquicktimecv akiradnews
+#
+#apt-get install --assume-yes --download-only cinelerracv
+#dpkg --unpack /var/cache/apt/archives/cinelerracv*
+#
+## yes, we remove it even if not triggered otherwise it will be a problem as soon as
+## aptitude or dpkg -a (etc..) are called while still in the chroot.
+#grep -Ev shmmax /var/lib/dpkg/info/cinelerracv.postinst > tmpcine
+#mv tmpcine /var/lib/dpkg/info/cinelerracv.postinst
 
