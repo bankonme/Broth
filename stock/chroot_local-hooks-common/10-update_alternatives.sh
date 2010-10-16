73fca593248ade0e3f67a5eed509a13dd82cacaf
Dan Stowell <danstowell+launch@gmail.com>
2010-10-16 21:57:36 +0100

    firefox as default x-www-browser

--- stock/chroot_local-hooks-common/10-update_alternatives.sh
+++ stock/chroot_local-hooks-common/10-update_alternatives.sh
@@ -6,3 +6,8 @@ update-alternatives --set pager /usr/bin/most
 
 # note: in puredyne 9.11 we used urxvtcd (client/server) but due to debian #481123 that's unavailable
 update-alternatives --set x-terminal-emulator /usr/bin/urxvt
+
+# links is lovely but...
+update-alternatives --set x-www-browser /usr/bin/firefox
+
+
