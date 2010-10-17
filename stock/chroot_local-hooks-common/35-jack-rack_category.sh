31f5a2dd6abe0d219338a1ffde6f52f69ff89e07
Dan Stowell <danstowell+launch@gmail.com>
2010-10-17 13:09:24 +0100

    category patches, update for upstream changes to files

--- stock/chroot_local-hooks-common/35-jack-rack_category.sh
+++ stock/chroot_local-hooks-common/35-jack-rack_category.sh
@@ -3,7 +3,7 @@
 if [ -e "/usr/share/applications/jack-rack.desktop" ]
 then
 
-echo "9c9
+echo "8c8
 < Categories=GNOME;Application;AudioVideo;
 ---
 > Categories=GNOME;Application;AudioVideo;Audio;" | patch --no-backup-if-mismatch --forward /usr/share/applications/jack-rack.desktop
--- stock/chroot_local-hooks-common/35-jamin_category.sh
+++ stock/chroot_local-hooks-common/35-jamin_category.sh
@@ -3,7 +3,7 @@
 if [ -e "/usr/share/applications/jamin.desktop" ]
 then
 
-echo "8c8
+echo "10c10
 < Categories=Application;AudioVideo;
 ---
 > Categories=Application;AudioVideo;Audio;" | patch --no-backup-if-mismatch --forward /usr/share/applications/jamin.desktop
