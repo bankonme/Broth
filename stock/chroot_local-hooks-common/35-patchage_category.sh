04a615b83a90221263fa62e5cc2e056cea1f9a41
Aymeric Mansoux <am-launchpad@kuri.mu>
2011-02-04 22:27:13 +0000

    patchage does not need category patch anymore

--- stock/chroot_local-hooks-common/35-patchage_category.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -e "/usr/share/applications/patchage.desktop" ]
-then
-    echo "11c11
-< Categories=Application;AudioVideo;
----
-> Categories=Application;AudioVideo;Audio;" | patch --no-backup-if-mismatch --forward /usr/share/applications/patchage.desktop
-fi
